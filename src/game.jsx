f82ec807b072e7e37cf2766ae5de530d5ab47e2f
Manasa Rao <drmanasa18.rao@gmail.com>
2019-12-23 21:09:52 +0100

    adds all logic of the game

--- src/game.jsx
+++ src/game.jsx
@@ -5,35 +5,77 @@ class Game extends React.Component {
         super()
         this.state = { 
             player_num: 1,
+            count: 0,
             player_select: [null,null,null,null,null,null,null,null,null]
         }
         this.handleClick = this.handleClick.bind(this)
+        this.handleReset = this.handleReset.bind(this)
+    }
+
+    handleReset (){
+        this.setState({ 
+            player_num: 1,
+            count: 0,
+            player_select: [null,null,null,null,null,null,null,null,null]
+        })
     }
 
     handleClick (num)  {
         let arry = this.state.player_select;
-        if (this.state.player_select[num]===null){
-        
-        if(this.state.player_num === 1) {
-            arry[num] = "X"; 
-            this.setState({player_num: 2,player_select: arry})
-        }
-        else {
-            arry[num] = "O";
-            this.setState({player_num: 1,player_select: arry})
-    }   
+        if (this.state.player_select[num]===null && (this.checkResult() === 0)){
+            if(this.state.player_num === 1) {
+                arry[num] = "X"; 
+                this.setState(prevState => {return {count: prevState.count+1, player_num: 2,player_select: arry}})
+            }
+            else {
+                arry[num] = "O";
+                this.setState(prevState => {return {count: prevState.count+1,player_num: 1,player_select: arry}})
+            }   
         }
     }
-    render(){
-        let finalResult ="Game over!"
-        for (var i = 0; i < 9; i++) {
-            if(this.state.player_select[i] === null){
-                finalResult = "";
+    checkResult () {
+        const combination = [
+            [0,1,2],
+            [3,4,5],
+            [6,7,8],
+            [0,4,8],
+            [2,4,6],
+            [0,3,6],
+            [1,4,7],
+            [2,5,8]
+        ]
+        for(let i=0;i<combination.length;i++){
+            const [a,b,c] = combination[i];
+            if (this.state.player_select[a]===this.state.player_select[b] 
+                && this.state.player_select[a]===this.state.player_select[c]
+                )
+            {
+                if(this.state.player_select[a] === "X") {
+                    return(1)
+                }
+                else if(this.state.player_select[b] === "O") {
+                    return(2)
+                }
             }
+        }
+        return(0)
+    }
+    render(){
+        let finalResult;
+        let winPlayer = this.checkResult();
+        console.log(winPlayer)
+        if(winPlayer !== 0) {
+            finalResult = (winPlayer == 1) ? "Player 1 wins" : "Player 2 Wins";
+        }
+        else if(this.state.count == 9){
+            finalResult = "GAME OVER"
+        }
+        else {
+            finalResult = null;
         }
         return( 
         <div >
-            <h3> Now player {this.state.player_num}'s' Turn</h3>
+            {finalResult === null && <h3> Now player {this.state.player_num}'s' Turn</h3>}
             <button className="box" onClick={() => this.handleClick(0)}>{this.state.player_select[0]}</button>
             <button className="box" onClick={() => this.handleClick(1)}>{this.state.player_select[1]}</button>
             <button className="box" onClick={() => this.handleClick(2)}>{this.state.player_select[2]}</button>
@@ -46,6 +88,7 @@ class Game extends React.Component {
             <button className="box" onClick={() => this.handleClick(7)}>{this.state.player_select[7]}</button>
             <button className="box" onClick={() => this.handleClick(8)}>{this.state.player_select[8]}</button>
             <h1>{finalResult}</h1>
+            <button onClick={this.handleReset}>Play Again!</button>
         </div>
         )
     }
